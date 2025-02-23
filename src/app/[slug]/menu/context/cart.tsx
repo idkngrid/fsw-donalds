2b1b505805d1990c77e2da1aa4c58f719705e409
idkngrid <ingrid.camargopr@gmail.com>
2025-02-23 18:42:03 -0300

    feat: ensure same product can be added to cart more than once

--- src/app/[slug]/menu/context/cart.tsx
+++ src/app/[slug]/menu/context/cart.tsx
@@ -31,9 +31,25 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   };
 
   const addProduct = (product: CartProduct) => {
-    setProducts((prev) => [...prev, product]);
-  };
+    const isProductAlreadyOnCart = products.some(
+      (prevProduct) => prevProduct.id === product.id,
+    );
 
+    if (!isProductAlreadyOnCart) {
+      return setProducts((prev) => [...prev, product]);
+    }
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id === product.id) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity + product.quantity,
+          };
+        }
+        return prevProduct;
+      });
+    });
+  };
   return (
     <CartContext.Provider
       value={{
