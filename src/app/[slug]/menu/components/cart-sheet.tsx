aa9d712a2b1a515241df9d77f099ca3eed8f27bb
idkngrid <ingrid.camargopr@gmail.com>
2025-02-24 19:00:17 -0300

    feat: add initial finish order dialog component

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Sheet,
@@ -12,6 +11,7 @@ import { formatCurrency } from "@/helpers/format-currency";
 
 import { CartContext } from "../context/cart";
 import CartProductItem from "./cart-product-item";
+import FinishOrderButton from "./finish-order-button";
 
 const CartSheet = () => {
   const { isOpen, toggleCart, products, total } = useContext(CartContext);
@@ -36,7 +36,7 @@ const CartSheet = () => {
               </div>
             </CardContent>
           </Card>
-          <Button className="w-full rounded-full">Finalizar pedido</Button>
+          <FinishOrderButton />
         </div>
       </SheetContent>
     </Sheet>
